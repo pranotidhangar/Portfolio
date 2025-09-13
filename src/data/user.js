237a7c525e4a92c639e7acd80a348e41a704d258
Pranoti Dhnagar <131478879+pranotidhangar@users.noreply.github.com>
2025-09-13 22:29:25 +0530

    Updated portfolio

--- src/data/user.js
+++ src/data/user.js
@@ -17,9 +17,9 @@ const INFO = {
 	},
 
 	homepage: {
-		title: "Frontend Developer and Python Developer, and ECE Engineer.",
+		title: "Java Full Stack Developer, and ECE Engineer.",
 		description:
-			"I am a web developer with expertise in ReactJs. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+			"I am a Java Full Stack Developer with expertise in building scalable, secure, and reliable web applications using Java, Spring Boot, Hibernate, React.js, and other modern frameworks and technologies. I have hands-on experience in both frontend and backend development, including designing RESTful APIs, working with relational databases, and implementing responsive user interfaces. I enjoy solving complex problems and continuously learning new technologies to enhance my skill set. I am passionate about writing high-quality, maintainable code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a full stack developer.",
 	},
 
 	about: {
@@ -36,7 +36,7 @@ const INFO = {
 
 	projects: [
 		{
-			title: "Project 1",
+			title: "Ecommerce Website (React)",
 			description:
 				"An ecommerce website is an online platform where businesses sell products or services directly to consumers over the internet. It typically includes features such as product listings, shopping carts, secure payment gateways, and often incorporates user accounts for personalized shopping experiences.",
 			logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyp7FFYRwNKbTumOgu4zGIJOQpv8y0ngkLHw&s",
@@ -45,16 +45,25 @@ const INFO = {
 		},
 
 		{
-			title: "Project 2",
+			title: "Inventory Management System(Spring Boot)",
 			description:
-				"The Recipe App is a user-friendly web application that allows users to manage their personal collection of recipes. Users can easily add new recipes by uploading an image and providing a name and description. Additionally, the app provides functionality to update existing recipes and delete recipes that are no longer needed. This ensures users have complete control over their recipe collection.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+				"The Inventory Management System (IMS) is a web-based application built with Spring Boot that helps businesses manage and track stock, sales, and purchases. It ensures accurate inventory records, reduces manual errors, and streamlines operations for better decision-making.",
+			logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1RNgloYivDxu-m0uthmQb78H2ULQhv94GZw&s",
 			linkText: "View Project",
-			link: "https://github.com/pranotidhangar/Receipe_App/",
+			link: "https://github.com/pranotidhangar/Inventory-Management-System",
 		},
 
 		{
-			title: "Project 3",
+			title: "Student Management System (Servlet,MySQL)",
+			description:
+				"The Student Management System is a web-based application built using Servlet, MySQL, HTML, and CSS that allows efficient management of student data. It enables adding, updating, deleting, and viewing student records with a simple and user-friendly interface.",
+			logo:"https://upload.wikimedia.org/wikipedia/en/3/30/Java_programming_language_logo.svg",
+			linkText: "View Project",
+			link: "https://github.com/pranotidhangar/Servlet_CRUD_Project",
+		},
+
+		{
+			title: "Blog Website (HTML,CSS)",
 			description:
 				"A blog website offering a platform for sharing insightful articles, personal stories, and expert opinions across various topics, fostering engagement and community interaction. Perfect for readers seeking fresh perspectives and writers looking to connect with a broader audience.",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
@@ -63,16 +72,16 @@ const INFO = {
 		},
 
 		{
-			title: "Project 4",
+			title: "Weather App (HTML,CSS,JS)",
 			description:
-				"A weather app that provides accurate forecasts, real-time updates, and customizable alerts to help you plan your day with confidence.",
+				"The Weather App is a web-based application developed using HTML, CSS, and JavaScript that provides real-time weather updates. It allows users to search for any city and view details like temperature, humidity, and weather conditions in an interactive interface.",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
 			linkText: "View Project",
 			link: "https://github.com/pranotidhangar/ClimateTrack",
 		},
 
 		{
-			title: "Project 5",
+			title: "Real Time Chat App (React and JS)",
 			description:
 				"A real-time chat app built using React and JavaScript, offering seamless communication with instant messaging capabilities. The app features a dynamic user interface, enabling users to engage in one-on-one or group conversations with real-time updates.",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
